665cfe9fdf82acc65d114b343c487dfe5dbd9da5
Batka Nergui <batka@batkas-mbp.mynetworksettings.com>
2024-10-15 11:43:05 -0500

    set up pages and routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BlogPage,
+  BlogsPage,
+  HomePage,
+  NotFoundPage,
+  ContactUsPage,
+} from "./pages";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route path="/blogs" element={<div>All Blog Page</div>} />
-        <Route path="/blogs/:id" element={<div>Home Page</div>} />
-        <Route path="/contact-us" element={<div>Contact Page</div>} />
-        <Route path="/404" element={<div>Page not found</div>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blogs" element={<BlogsPage />} />
+        <Route path="/blogs/:id" element={<BlogPage />} />
+        <Route path="/contact-us" element={<ContactUsPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
